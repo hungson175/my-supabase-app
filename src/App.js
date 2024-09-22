61ad85446a5292c29c6c5ff3194d3c67fdcd88c6
hungson175 <sphamhung@gmail.com>
2024-09-22 14:39:19 +0700

    Update code

--- src/App.js
+++ src/App.js
@@ -24,7 +24,7 @@ function App() {
       <h1>Users:</h1>
       <ul>
         {users.map((user) => (
-          <li key={user.id}>{user.name}</li>
+          <li key={user.id}>{user.username}</li>
         ))}
       </ul>
     </div>
